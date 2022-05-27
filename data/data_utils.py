634ec5cb33f583cf4b9fcd87b51770a29ef718d1
alessandro <alessandro.melchiorre@jku.at>
2022-05-27 13:43:39 +0200

    Move data utils to a single file (data_utils.py)
    Refactored the dataset from dataset.py Three types of dataset are now available. In particular, there is now RecDataset is a non-specialized version of TrainRecDataset and EvalRecDataset

--- data/data_utils.py
+++ data/data_utils.py
@@ -1,11 +1,15 @@
 import enum
 
+from torch.utils.data import DataLoader
+
+from data.dataset import TrainRecDataset, FullEvalDataset
+
 INF_STR = "{:10d} entries {:7d} users {:7d} items for {}"
 LOG_FILT_DATA_PATH = "log_filtering_data.txt"
 
 
 # Note that the name of the dataset should correspond to a folder in data. e.g. ml1m has a corresponding folder in /data
-class RecDatasetsEnum(enum.Enum):
+class DatasetsEnum(enum.Enum):
     ml1m = enum.auto()
     ml10m = enum.auto()
     amazonvid = enum.auto()
@@ -16,3 +20,38 @@ def print_and_log(log_file, n_lhs, n_users, n_items, text):
     info_string = INF_STR.format(n_lhs, n_users, n_items, text)
     log_file.write(info_string + '\n')
     print(info_string)
+
+
+def get_dataloader(conf: dict, split_set: str) -> DataLoader:
+    """
+        Returns the dataloader associated to the configuration in conf
+    """
+
+    if split_set == 'train':
+        return DataLoader(
+            TrainRecDataset(
+                data_path=conf['dataset_path'],
+                n_neg=conf['neg_train'],
+                neg_sampling_strategy=conf['train_neg_strategy'],
+            ),
+            batch_size=conf['train_batch_size'],
+            shuffle=True,
+            num_workers=conf['running_settings']['n_workers']
+        )
+    elif split_set == 'val':
+        return DataLoader(
+            FullEvalDataset(
+                data_path=conf['dataset_path'],
+                split_set='val',
+            ),
+            batch_size=conf['eval_batch_size']
+        )
+
+    elif split_set == 'test':
+        return DataLoader(
+            FullEvalDataset(
+                data_path=conf['dataset_path'],
+                split_set='test',
+            ),
+            batch_size=conf['eval_batch_size']
+        )
--- data/dataset.py
+++ data/dataset.py
@@ -21,7 +21,74 @@ should be the following csv files:
 """
 
 
-class TrainRecDataset(data.Dataset):
+class RecDataset(data.Dataset):
+    """
+    Dataset to hold Recommender System data and train collaborative filtering algorithms. It allows iteration over the
+    dataset of positive interactions.
+    """
+
+    def __init__(self, data_path: str, split_set: str):
+        """
+        :param data_path: Path to the directory with listening_history_train.csv, user_idxs.csv, item_idxs.csv
+        """
+        assert split_set in ['train', 'val', 'test'], f'<{split_set}> is not a valid value for split set!'
+        self.data_path = data_path
+        self.split_set = split_set
+
+        self.n_users = None
+        self.n_items = None
+
+        self.iteration_matrix = None
+
+        self.pop_distribution = None
+
+        self.load_data()
+
+        self.name = "RecDataset"
+        print(f'Built {self.name} module \n'
+              f'- data_path: {self.data_path} \n'
+              f'- split_set: {self.split_set} \n'
+              f'- n_users: {self.n_users} \n'
+              f'- n_items: {self.n_items} \n'
+              f'- n_interactions: {self.iteration_matrix.nnz} \n')
+
+    def load_data(self):
+        print('Loading data')
+
+        user_idxs = pd.read_csv(os.path.join(self.data_path, 'user_idxs.csv'))
+        item_idxs = pd.read_csv(os.path.join(self.data_path, 'item_idxs.csv'))
+
+        self.n_users = len(user_idxs)
+        self.n_items = len(item_idxs)
+
+        lhs = pd.read_csv(os.path.join(self.data_path, f'listening_history_{self.split_set}.csv'))
+
+        if self.split_set == 'train':
+            type_matrix = sp.coo_matrix  # During training we iterate over the single interactions
+        else:
+            type_matrix = sp.csr_matrix  # During evaluation we iterate user-wise
+
+        self.iteration_matrix = type_matrix(
+            (np.ones(len(lhs), dtype=np.int16), (lhs.user_idx, lhs.item_idx)),
+            shape=(self.n_users, self.n_items))
+
+        # Computing the popularity distribution
+        item_popularity = np.array(self.iteration_matrix.sum(axis=0)).flatten()
+        self.pop_distribution = item_popularity / item_popularity.sum()
+
+        print('End loading data')
+
+    def __len__(self):
+        return self.iteration_matrix.nnz
+
+    def __getitem__(self, index):
+        user_idx = self.iteration_matrix.row[index].astype('int64')
+        item_idx = self.iteration_matrix.col[index].astype('int64')
+
+        return user_idx, item_idx, 1.
+
+
+class TrainRecDataset(RecDataset):
     """
     Dataset to hold Recommender System data and train collaborative filtering algorithms. It allows iteration over the
     dataset of positive interaction and offers negative sampling functionalities (e.g. for each positive pair,
@@ -41,20 +108,16 @@ class TrainRecDataset(data.Dataset):
         :param squashing_factor_pop_sampling: Squashing factor for the popularity sampling. Ignored if neg_sampling_strategy = 'uniform'
         """
 
+        super().__init__(data_path, 'train')
         assert neg_sampling_strategy in ['uniform', 'popular'], \
             f'<{neg_sampling_strategy}> is not a valid negative sampling strategy!'
         assert squashing_factor_pop_sampling >= 0, 'Squashing factor for popularity sampling should be positive!'
 
-        self.data_path = data_path
         self.n_neg = n_neg
         self.neg_sampling_strategy = neg_sampling_strategy
         self.squashing_factor_pop_sampling = squashing_factor_pop_sampling
 
-        self.n_users = None
-        self.n_items = None
-
-        self.iteration_matrix = None
-        self.sampling_matrix = None
+        self.sampling_matrix = sp.csr_matrix(self.iteration_matrix)
 
         if self.neg_sampling_strategy == 'uniform':
             self.neg_sampler = self._neg_sample_uniform
@@ -63,41 +126,11 @@ class TrainRecDataset(data.Dataset):
         else:
             raise ValueError('Error with the choice of neg_sampling strategy')
 
-        self.pop_distribution = None
-
-        self.load_data()
-
-        print(f'Built TrainRecDataset module \n'
-              f'- data_path: {self.data_path} \n'
-              f'- n_users: {self.n_users} \n'
-              f'- n_items: {self.n_items} \n'
-              f'- n_interactions: {self.iteration_matrix.nnz} \n'
+        self.name = 'TrainRecDataset'
+        print(f'Built {self.name} module \n'
               f'- n_neg: {self.n_neg} \n'
               f'- neg_sampling_strategy: {self.neg_sampling_strategy} \n'
               f'- squashing_factor_pop_sampling: {self.squashing_factor_pop_sampling} \n')
-
-    def load_data(self):
-        print('Loading data')
-
-        user_idxs = pd.read_csv(os.path.join(self.data_path, 'user_idxs.csv'))
-        item_idxs = pd.read_csv(os.path.join(self.data_path, 'item_idxs.csv'))
-
-        self.n_users = len(user_idxs)
-        self.n_items = len(item_idxs)
-
-        train_lhs = pd.read_csv(os.path.join(self.data_path, 'listening_history_train.csv'))
-
-        self.sampling_matrix = sp.csr_matrix(
-            (np.ones(len(train_lhs), dtype=np.int16), (train_lhs.user_idx, train_lhs.item_idx)),
-            shape=(self.n_users, self.n_items))
-
-        # Computing the popularity distribution (see _neg_sample_popular)
-        item_popularity = np.array(self.sampling_matrix.sum(axis=0)).flatten()
-        self.pop_distribution = item_popularity / item_popularity.sum()
-
-        self.iteration_matrix = sp.coo_matrix(self.sampling_matrix)
-
-        print('End loading data')
 
     def _neg_sample_uniform(self, row_idx: int, n_neg: int) -> np.array:
         """
@@ -136,13 +169,9 @@ class TrainRecDataset(data.Dataset):
         sampled = np.random.choice(np.arange(self.n_items), n_neg, replace=False, p=p)
         return sampled
 
-    def __len__(self):
-        return self.iteration_matrix.nnz
-
     def __getitem__(self, index):
 
-        user_idx = self.iteration_matrix.row[index].astype('int64')
-        item_idx_pos = self.iteration_matrix.col[index]
+        user_idx, item_idx_pos, _ = super().__getitem__(index)
 
         # Negative sampling
         neg_samples = self.neg_sampler(user_idx, self.n_neg)
@@ -155,7 +184,7 @@ class TrainRecDataset(data.Dataset):
         return user_idx, item_idxs, labels
 
 
-class FullEvalDataset(data.Dataset):
+class FullEvalDataset(RecDataset):
     """
     Dataset to hold Recommender System data and evaluate collaborative filtering algorithms. It allows iteration over
     all the users and compute the scores for all items (FullEvaluation).
@@ -168,48 +197,21 @@ class FullEvalDataset(data.Dataset):
         :param avoid_zeros_users: Whether the dataset will also return users with no items on val/test data
         """
 
-        assert split_set in ['val', 'test'], f'<{split_set}> is not a valid value for split set!'
+        super().__init__(data_path, split_set)
 
-        self.data_path = data_path
-        self.split_set = split_set
         self.avoid_zeros_users = avoid_zeros_users
 
-        self.n_users = None
-        self.n_items = None
-
-        self.evaluation_matrix = None
         self.idx_to_user = None
+        if self.avoid_zeros_users:
+            items_per_users = self.iteration_matrix.getnnz(axis=-1)
+            self.idx_to_user = np.where(items_per_users > 0)[0]
 
         self.load_data()
 
-        print(f'Built FullEvalDataset module \n'
-              f'- data_path: {self.data_path} \n'
-              f'- split_set: {self.split_set} \n'
-              f'- avoid_zeros_users: {self.avoid_zeros_users} \n'
-              f'- n_users: {self.n_users} \n'
-              f'- n_items: {self.n_items} \n'
-              f'- n_interactions: {self.evaluation_matrix.nnz} \n')
+        self.name = 'FullEvalDataset'
 
-    def load_data(self):
-        print('Loading data')
-
-        user_idxs = pd.read_csv(os.path.join(self.data_path, 'user_idxs.csv'))
-        item_idxs = pd.read_csv(os.path.join(self.data_path, 'item_idxs.csv'))
-
-        self.n_users = len(user_idxs)
-        self.n_items = len(item_idxs)
-
-        eval_lhs = pd.read_csv(os.path.join(self.data_path, f'listening_history_{self.split_set}.csv'))
-
-        self.evaluation_matrix = sp.csr_matrix(
-            (np.ones(len(eval_lhs), dtype=np.int16), (eval_lhs.user_idx, eval_lhs.item_idx)),
-            shape=(self.n_users, self.n_items))
-
-        if self.avoid_zeros_users:
-            items_per_users = self.evaluation_matrix.getnnz(axis=-1)
-            self.idx_to_user = np.where(items_per_users > 0)[0]
-
-        print('End loading data')
+        print(f'Built {self.name} module \n'
+              f'- avoid_zeros_users: {self.avoid_zeros_users} \n')
 
     def __len__(self):
         if self.avoid_zeros_users:
@@ -220,7 +222,7 @@ class FullEvalDataset(data.Dataset):
     def __getitem__(self, user_index):
         if self.avoid_zeros_users:
             user_index = self.idx_to_user[user_index]
-        return user_index, np.arange(self.n_items), self.evaluation_matrix[user_index].toarray().squeeze().astype(
+        return user_index, np.arange(self.n_items), self.iteration_matrix[user_index].toarray().squeeze().astype(
             'float32')
 
 
