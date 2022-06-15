7d12459e4154ba582383c6f9c5f59c647867b3f0
alessandro <alessandro.melchiorre@jku.at>
2022-06-15 11:22:04 +0200

    First Version of starting script and experiment helper. Added conf parser to add default parameters when not specified

---
+++ conf/conf_parser.py
@@ -0,0 +1,161 @@
+import os.path
+
+import yaml
+
+from algorithms.algorithms_utils import AlgorithmsEnum
+from algorithms.base_classes import SGDBasedRecommenderAlgorithm
+from data.data_utils import DatasetsEnum
+from train.rec_losses import RecommenderSystemLossesEnum
+from utilities.utils import generate_id
+
+DEF_NEG_TRAIN = 4
+DEF_NEG_STRATEGY = 'uniform'
+DEF_TRAIN_BATCH_SIZE = 64
+DEF_EVAL_BATCH_SIZE = 64
+DEF_NUM_WORKERS = 2
+DEF_SEED = 64
+DEF_N_EPOCHS = 50
+DEF_USE_WANDB = True
+DEF_MODEL_SAVE_PATH = './saved_models'
+DEF_LEARNING_RATE = 1e-3
+DEF_WEIGHT_DECAY = 0
+DEF_OPTIMIZER = 'adam'
+DEF_REC_LOSS = 'bce'
+DEF_LOSS_AGGREGATOR = 'mean'
+DEF_DEVICE = 'cpu'
+DEF_OPTIMIZING_METRIC = 'ndcg@10'
+DEF_MAX_PATIENCE = DEF_N_EPOCHS
+
+
+def parse_yaml(conf_path: str) -> dict:
+    assert os.path.isfile(conf_path), f'Configuration File {conf_path} not found!'
+
+    with open(conf_path, 'r') as conf_file:
+        conf = yaml.safe_load(conf_file)
+    print(' --- Configuration Loaded ---')
+    return conf
+
+
+def save_yaml(conf_path: str, conf: dict):
+    conf_path = os.path.join(conf_path, 'conf.yml')
+
+    with open(conf_path, 'w') as conf_file:
+        yaml.dump(conf, conf_file)
+    print(' --- Configuration Saved ---')
+
+
+def parse_conf(conf: dict, alg: AlgorithmsEnum, dataset: DatasetsEnum) -> dict:
+    """
+    It sets basic parameters of the configurations and provides default parameters
+    """
+    assert 'data_path' in conf, "Data path is missing from the configuration file"
+
+    conf['alg'] = alg.name
+    conf['time_run'] = generate_id()
+    conf['dataset'] = dataset.name
+    conf['data_path'] = conf['data_path']
+    conf['dataset_path'] = os.path.join(conf['data_path'], conf['dataset'])
+
+    # Adding default parameters
+    added_parameters_list = []
+
+    if 'model_save_path' not in conf:
+        conf['model_save_path'] = DEF_MODEL_SAVE_PATH
+        added_parameters_list.append(f"model_save_path={conf['model_save_path']}")
+
+    conf['model_path'] = os.path.join(conf['model_save_path'], "{}-{}".format(alg.name, dataset.name), conf['time_run'])
+    os.makedirs(conf['model_path'], exist_ok=True)
+
+    if 'running_settings' not in conf:
+        conf['running_settings'] = dict()
+
+    if 'eval_batch_size' not in conf:
+        conf['eval_batch_size'] = DEF_EVAL_BATCH_SIZE
+        added_parameters_list.append(f"eval_batch_size={conf['eval_batch_size']}")
+
+    if 'seed' not in conf['running_settings']:
+        conf['running_settings']['seed'] = DEF_SEED
+        added_parameters_list.append(f"seed={conf['running_settings']['seed']}")
+
+    if 'use_wandb' not in conf['running_settings']:
+        conf['running_settings']['use_wandb'] = DEF_USE_WANDB
+        added_parameters_list.append(f"use_wandb={conf['running_settings']['use_wandb']}")
+
+    if issubclass(alg.value, SGDBasedRecommenderAlgorithm):
+        if 'neg_train' not in conf:
+            conf['neg_train'] = DEF_NEG_TRAIN
+            added_parameters_list.append(f"neg_train={conf['neg_train']}")
+
+        if 'train_neg_strategy' not in conf:
+            conf['train_neg_strategy'] = DEF_NEG_STRATEGY
+            added_parameters_list.append(f"train_neg_strategy={conf['train_neg_strategy']}")
+
+        if 'train_batch_size' not in conf:
+            conf['train_batch_size'] = DEF_TRAIN_BATCH_SIZE
+            added_parameters_list.append(f"train_batch_size={conf['train_batch_size']}")
+
+        if 'n_epochs' not in conf:
+            conf['n_epochs'] = DEF_N_EPOCHS
+            added_parameters_list.append(f"n_epochs={conf['n_epochs']}")
+        else:
+            assert conf['n_epochs'] > 0, f"Number of epochs ({conf['n_epochs']}) should be positive"
+
+        if 'lr' not in conf:
+            conf['lr'] = DEF_LEARNING_RATE
+            added_parameters_list.append(f"lr={conf['lr']}")
+        else:
+            assert conf['lr'] > 0, f"Learning rate ({conf['lr']}) should be positive"
+
+        if 'wd' not in conf:
+            conf['wd'] = DEF_WEIGHT_DECAY
+            added_parameters_list.append(f"wd={conf['wd']}")
+        else:
+            assert conf['lr'] > 0, f"Weight Decay ({conf['wd']}) should be positive"
+
+        if 'optimizer' not in conf:
+            conf['optimizer'] = DEF_OPTIMIZER
+            added_parameters_list.append(f"optimizer={conf['optimizer']}")
+        else:
+            assert conf['optimizer'] in ['adam', 'adagrad'], f"Optimizer ({conf['optimizer']}) not implemented"
+
+        if 'rec_loss' not in conf:
+            conf['rec_loss'] = DEF_REC_LOSS
+            added_parameters_list.append(f"rec_loss={conf['rec_loss']}")
+        else:
+            assert conf['rec_loss'] in [rec_loss.name for rec_loss in
+                                        RecommenderSystemLossesEnum], f"Rec loss ({conf['rec_loss']}) not implemented"
+
+        if 'loss_aggregator' not in conf:
+            conf['loss_aggregator'] = DEF_LOSS_AGGREGATOR
+            added_parameters_list.append(f"loss_aggregator={conf['loss_aggregator']}")
+        else:
+            assert conf['loss_aggregator'] in ['mean',
+                                               'sum'], f"Loss aggregator ({conf['loss_aggregator']}) not implemented"
+
+        if 'device' not in conf:
+            conf['device'] = DEF_DEVICE
+            added_parameters_list.append(f"device={conf['device']}")
+        else:
+            assert conf['device'] in ['cpu',
+                                      'cuda'], f"Device ({conf['device']}) not available"
+
+        if 'optimizing_metric' not in conf:
+            conf['optimizing_metric'] = DEF_OPTIMIZING_METRIC
+            added_parameters_list.append(f"optimizing_metric={conf['optimizing_metric']}")
+
+        if 'max_patience' not in conf:
+            conf['max_patience'] = DEF_MAX_PATIENCE
+            added_parameters_list.append(f"max_patience={conf['max_patience']}")
+        else:
+            assert 0 < conf['max_patience'] < conf[
+                'n_epochs'], f"Max patience {conf['max_patience']} should be between 0 and {conf['n_epochs']}"
+
+        if 'n_workers' not in conf['running_settings']:
+            conf['running_settings']['n_workers'] = DEF_NUM_WORKERS
+            added_parameters_list.append(f"n_workers={conf['running_settings']['n_workers']}")
+
+    print('Added these default parameters: ', ", ".join(added_parameters_list))
+    print('For more detail, see conf/conf_parser.py')
+    print('\n\n')
+
+    return conf
--- experiment_helper.py
+++ experiment_helper.py
@@ -1,256 +1,105 @@
-import os
-from collections import defaultdict
-
-import numpy as np
+import typing
 import wandb
-from ray import tune
-from ray.tune.integration.wandb import WandbLoggerCallback
-from ray.tune.schedulers import ASHAScheduler
-from ray.tune.stopper import CombinedStopper, TrialPlateauStopper
-from ray.tune.suggest.hyperopt import HyperOptSearch
 
-from algorithms.base_classes import SGDBasedRecommenderAlgorithm
-from data.dataset import get_recdataset_dataloader
-from hyper_params import alg_param
-from utilities.consts import NEG_VAL, SINGLE_SEED, PROJECT_NAME, WANDB_API_KEY_PATH, DATA_PATH, OPTIMIZING_METRIC, \
-    SEED_LIST
-from utilities.enums import RecAlgorithmsEnum, RecDatasetsEnum
-from utilities.eval import evaluate_recommender_algorithm, evaluate_naive_algorithm
-from utilities.trainer import ExperimentConfig, Trainer
-from utilities.utils import generate_id, reproducible, NoImprovementsStopper, KeepOnlyTopTrials
+from algorithms.algorithms_utils import AlgorithmsEnum
+from algorithms.base_classes import SGDBasedRecommenderAlgorithm, SparseMatrixBasedRecommenderAlgorithm
+from algorithms.naive_algs import PopularItems
+from conf.conf_parser import parse_yaml, parse_conf, save_yaml
+from data.data_utils import DatasetsEnum, get_dataloader
+from data.dataset import RecDataset
+from eval.eval import evaluate_recommender_algorithm
+from train.trainer import Trainer
+from utilities.utils import reproducible
+from wandb_conf import PROJECT_NAME, ENTITY_NAME
 
 
-def load_data(conf: dict, split_set: str, **kwargs):
-    if split_set == 'train':
-        train_loader = get_recdataset_dataloader(
-            'inter',
-            data_path=conf['data_path'],
-            split_set='train',
-            n_neg=conf['neg_train'] if "neg_train" in conf else 4,
-            neg_strategy=conf['train_neg_strategy'] if 'train_neg_strategy' in conf else 'uniform',
-            batch_size=conf['batch_size'] if 'batch_size' in conf else 64,
-            shuffle=True,
-            num_workers=kwargs['n_workers']
-        )
-        return train_loader
-    elif split_set == 'val':
-        val_loader = get_recdataset_dataloader(
-            'inter',
-            data_path=conf['data_path'],
-            split_set='val',
-            n_neg=NEG_VAL,
-            neg_strategy=conf['eval_neg_strategy'],
-            batch_size=conf['val_batch_size'],
-        )
-        return val_loader
-    elif split_set == 'test':
-        test_loader = get_recdataset_dataloader(
-            'inter',
-            data_path=conf['data_path'],
-            split_set='test',
-            n_neg=NEG_VAL,
-            neg_strategy=conf['eval_neg_strategy'],
-            batch_size=conf['val_batch_size'],
-        )
+def run_train_val(alg: AlgorithmsEnum, dataset: DatasetsEnum, conf: typing.Union[str, dict]):
+    print('Starting Train-Val')
+    print(f'Algorithm is {alg.name} - Dataset is {dataset.name}')
 
-        return test_loader
+    if isinstance(conf, str):
+        conf = parse_yaml(conf)
+    conf = parse_conf(conf, alg, dataset)
 
+    if conf['running_settings']['use_wandb']:
+        wandb.init(project=PROJECT_NAME, entity=ENTITY_NAME, config=conf, tags=[alg.name, dataset.name],
+                   group=f'{alg.name} - {dataset.name} - train/val', name=conf['time_run'], job_type='train/val')
 
-def tune_training(conf: dict, checkpoint_dir=None):
-    """
-    Function executed by ray tune. It corresponds to a single trial.
-    """
+    reproducible(conf['running_settings']['seed'])
 
-    train_loader = load_data(conf, 'train', n_workers=conf['experiment_settings']['n_workers'])
-    val_loader = load_data(conf, 'val')
+    if issubclass(alg.value, SGDBasedRecommenderAlgorithm):
 
-    reproducible(conf['seed'])
+        train_loader = get_dataloader(conf, 'train')
+        val_loader = get_dataloader(conf, 'val')
 
-    alg = conf['alg'].value.build_from_conf(conf, train_loader.dataset)
-    with tune.checkpoint_dir(0) as checkpoint_dir:
+        alg = alg.value.build_from_conf(conf, train_loader.dataset)
 
-        if isinstance(alg, SGDBasedRecommenderAlgorithm):
+        # Validation happens within the Trainer
+        trainer = Trainer(alg, train_loader, val_loader, conf)
+        metrics_values = trainer.fit()
+        save_yaml(conf['model_path'], conf)
 
-            checkpoint_file = os.path.join(checkpoint_dir, 'best_model.pth')
-            conf['n_epochs'] = conf['experiment_settings']['n_epochs']
-            exp_conf = ExperimentConfig.build_from_conf(conf)
-            exp_conf.best_model_path = checkpoint_file
+    elif issubclass(alg.value, SparseMatrixBasedRecommenderAlgorithm):
+        train_dataset = RecDataset(conf['dataset_path'], 'train')
+        val_loader = get_dataloader(conf, 'val')
 
-            # Validation happens within Trainer
-            trainer = Trainer(alg, train_loader, val_loader, exp_conf)
-            trainer.fit()
+        alg = alg.value.build_from_conf(conf, train_dataset)
+        # -- Training --
+        alg.fit(train_dataset.iteration_matrix)
+        # -- Validation --
+        metrics_values = evaluate_recommender_algorithm(alg, val_loader)
 
-        else:
-            checkpoint_file = os.path.join(checkpoint_dir, 'best_model.npz')
-            # -- Training --
-            # todo: ensure that all algorithms have the following method!
-            alg.fit(train_loader.dataset.iteration_matrix)
+        alg.save_model_to_path(conf['model_path'])
+        save_yaml(conf['model_path'], conf)
 
-            # -- Validation --
-            metrics_values = evaluate_recommender_algorithm(alg, val_loader, conf['seed'] + 1)
-            tune.report(**metrics_values)
+        if conf['running_settings']['use_wandb']:
+            wandb.log(metrics_values)
+    elif alg in [AlgorithmsEnum.rand, AlgorithmsEnum.pop]:
 
-            # -- Save --
-            alg.save_model_to_path(checkpoint_file)
+        train_dataset = RecDataset(conf['dataset_path'], 'train')
+        val_loader = get_dataloader(conf, 'val')
 
-
-def run_train_val(conf: dict, run_name: str, **kwargs):
-    """
-    Runs the train and validation procedure.
-    """
-
-    # Hyperparameter Optimization
-    metric_name = OPTIMIZING_METRIC
-
-    # Search Algorithm
-    search_alg = HyperOptSearch(random_state_seed=conf['seed'])
-
-    if os.path.basename(conf['data_path']) == 'lfm2b1m':
-        scheduler = ASHAScheduler(grace_period=4)
+        alg = alg.value.build_from_conf(conf, train_dataset)
+        metrics_values = evaluate_recommender_algorithm(alg, val_loader)
+        if conf['running_settings']['use_wandb']:
+            wandb.log(metrics_values)
     else:
-        scheduler = None
+        raise ValueError(f'Training for {alg.value} has been not implemented')
 
-    # Logger
-    log_callback = WandbLoggerCallback(project=PROJECT_NAME, log_config=True, api_key_file=WANDB_API_KEY_PATH,
-                                       reinit=True, force=True, job_type='train/val', tags=run_name.split('_'))
-
-    keep_callback = KeepOnlyTopTrials(metric_name, n_tops=3)
-
-    # Stopper
-    stopper = CombinedStopper(
-        NoImprovementsStopper(metric_name, max_patience=10),
-        TrialPlateauStopper(metric_name, std=1e-3, num_results=5, grace_period=10)
-    )
-
-    # Other experiment's settings
-    conf['experiment_settings'] = kwargs
-
-    tune.register_trainable(run_name, tune_training)
-    analysis = tune.run(
-        run_name,
-        config=conf,
-        name=generate_id(prefix=run_name),
-        resources_per_trial={'gpu': kwargs['n_gpus']},
-        scheduler=scheduler,
-        search_alg=search_alg,
-        num_samples=kwargs['n_samples'],
-        callbacks=[log_callback, keep_callback],
-        metric=metric_name,
-        stop=stopper,
-        max_concurrent_trials=kwargs['n_concurrent'],
-        mode='max',
-        fail_fast=True,
-    )
-
-    best_value, best_checkpoint, best_config = keep_callback.get_best_trial()
-    # best_trial = analysis.get_best_trial(metric_name, 'max', scope='all')
-    # best_config = best_trial.config
-    # best_checkpoint = analysis.get_best_checkpoint(best_trial, metric_name, 'max')
-
-    print('Train and Val ended')
-    print(f'Best configuration is: \n {best_config}')
-    print(f'Best checkpoint is: \n {best_checkpoint}')
-
-    return best_config, best_checkpoint
+    return metrics_values, conf
 
 
-def run_test(run_name: str, best_config: dict, best_checkpoint=''):
-    """
-    Runs the test procedure. Notice that the evaluation of the recommendation uses conf['seed'] + 2!
-    """
-    test_loader = load_data(best_config, 'test')
+def run_test(alg: AlgorithmsEnum, dataset: DatasetsEnum, conf: typing.Union[str, dict]):
+    print('Starting Test')
+    print(f'Algorithm is {alg.name} - Dataset is {dataset.name}')
 
-    wandb.login()
-    wandb.init(project=PROJECT_NAME, group='test', config=best_config, name=run_name, force=True,
-               job_type='test', tags=run_name.split('_'))
+    if isinstance(conf, str):
+        conf = parse_yaml(conf)
 
-    # ---- Test ---- #
-    alg = best_config['alg'].value.build_from_conf(best_config, test_loader.dataset)
-    if isinstance(alg, SGDBasedRecommenderAlgorithm):
-        best_checkpoint = os.path.join(best_checkpoint, 'best_model.pth')
+    if conf['running_settings']['use_wandb']:
+        wandb.init(project=PROJECT_NAME, entity=ENTITY_NAME, config=conf, tags=[alg.name, dataset.name],
+                   group=f'{alg.name} - {dataset.name} - test', name=conf['time_run'], job_type='test', reinit=True)
+
+    test_loader = get_dataloader(conf, 'test')
+
+    if alg.value == PopularItems:
+        # Popular Items requires the popularity distribution over the items learned over the training data
+        alg = alg.value.build_from_conf(conf, RecDataset(conf['dataset_path'], 'train'))
     else:
-        best_checkpoint = os.path.join(best_checkpoint, 'best_model.npz')
-    alg.load_model_from_path(best_checkpoint)
-    if best_config['alg'] in [RecAlgorithmsEnum.pop, RecAlgorithmsEnum.rand]:
-        metrics_values = evaluate_naive_algorithm(alg, test_loader, best_config['seed'] + 2)
-    else:
-        metrics_values = evaluate_recommender_algorithm(alg, test_loader, best_config['seed'] + 2)
+        alg = alg.value.build_from_conf(conf, test_loader.dataset)
 
-    wandb.log(metrics_values)
+    alg.load_model_from_path(conf['model_path'])
 
-    wandb.finish()
-
-    return metrics_values
+    metrics_values = evaluate_recommender_algorithm(alg, test_loader)
+    if conf['running_settings']['use_wandb']:
+        wandb.log(metrics_values)
 
 
-def start_hyper(alg: RecAlgorithmsEnum, dataset: RecDatasetsEnum, seed: int = SINGLE_SEED, **kwargs) -> dict:
-    print('Starting Hyperparameter Optimization')
-    print(f'Dataset is {dataset.name} - Seed is {seed}')
-    print('N.B. Val seed is obtained by seed + 1, Test seed by seed + 2 ')
+def run_train_val_test(alg: AlgorithmsEnum, dataset: DatasetsEnum, conf_path: str):
+    print('Starting Train-Val-Test')
+    print(f'Algorithm is {alg.name} - Dataset is {dataset.name}')
 
-    # ---- Algorithm's parameters and hyperparameters ---- #
-    conf = alg_param[alg]
-    conf['alg'] = alg
-
-    # Dataset
-    conf['data_path'] = os.path.join(os.getcwd(), DATA_PATH, dataset.name)
-
-    # Seed
-    conf['seed'] = seed
-
-    # Hostname
-    host_name = os.uname()[1][:2]
-
-    run_name = f'{alg.name}_{dataset.name}_{host_name}_{seed}'
-
-    # ---- Train/Validation ---- #
-    best_config, best_checkpoint = None, ''
-    if alg not in [RecAlgorithmsEnum.pop, RecAlgorithmsEnum.rand]:
-        print('Start Train/Val')
-        best_config, best_checkpoint = run_train_val(conf, run_name, **kwargs)
-    else:
-        best_config = conf
-
-    print('Start Test')
-    metric_values = run_test(run_name, best_config, best_checkpoint)
-
-    print('End')
-    return metric_values
-
-
-def start_multiple_hyper(alg: RecAlgorithmsEnum, dataset: RecDatasetsEnum, **kwargs):
-    print('Starting Multi-Hyperparameter Optimization')
-    print(f'Dataset is {dataset.name} - Seeds are {SEED_LIST}')
-
-    # Accumulate the results in a dictionary: e.g. results_list['ndcg@10'] = [0.8,0.5,0.3]
-    results_dict = defaultdict(list)
-
-    # Carry out the experiment
-    for seed in SEED_LIST:
-        metric_values = start_hyper(alg, dataset, seed, **kwargs)
-        for key in metric_values:
-            results_dict[key].append(metric_values[key])
-
-    # Having collected all the values, carry out mean and std
-    aggr_results_dict = defaultdict(lambda x: 0)
-    for key, values_list in results_dict.items():
-        aggr_results_dict[f'{key} mu'] = np.mean(values_list)
-        aggr_results_dict[f'{key} sig'] = np.std(values_list)
-
-    # Log results
-    with open(WANDB_API_KEY_PATH) as wandb_file:
-        wandb_api_key = wandb_file.read()
-
-    run_name = f'{alg.name}_{dataset.name}'
-    wandb.login(key=wandb_api_key)
-    wandb.init(project=PROJECT_NAME, group='aggr_results', name=run_name, force=True, job_type='test',
-               tags=run_name.split('_'))
-
-    wandb.log(aggr_results_dict)
-    wandb.finish()
-
-
-def start_multi_dataset(alg: RecAlgorithmsEnum, **kwargs):
-    print('Starting Multi-dataset Multi-Hyperparameter Optimization')
-    for dataset in RecDatasetsEnum:
-        start_multiple_hyper(alg, dataset, **kwargs)
+    # ------ Run train and Val ------ #
+    metrics_values, conf = run_train_val(alg, dataset, conf_path)
+    # ------ Run test ------ #
+    run_test(alg, dataset, conf)
